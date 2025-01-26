4d400b7ee890e5f983a7c933c859a8186454b411
Thierry Moudiki <thierry.moudiki@gmail.com>
2025-01-26 05:43:58 +0100

    add example

---
+++ examples/examples_et.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.utils.discovery import all_estimators
+from sklearn.datasets import load_diabetes, load_iris, load_breast_cancer, load_wine
+from sklearn.linear_model import Ridge, RidgeCV
+from sklearn.tree import ExtraTreeRegressor
+from sklearn.model_selection import train_test_split
+from genbooster.genboosterclassifier import BoosterClassifier
+from genbooster.genboosterregressor import BoosterRegressor
+from sklearn.metrics import mean_squared_error
+from tqdm import tqdm
+from time import time
+
+print("\n\n BoosterRegressor boston dataset -----")
+
+url = "https://raw.githubusercontent.com/Techtonique/datasets/refs/heads/main/tabular/regression/boston_dataset2.csv"
+df = pd.read_csv(url)
+print(df.head())
+
+X = df.drop(columns=['target', 'training_index'])
+y = df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=142)
+
+
+# 2.6972142372651713
+regr = BoosterRegressor(base_estimator=ExtraTreeRegressor())
+start = time()
+regr.fit(X_train, y_train)
+end = time()
+print(f"Time taken: {end - start} seconds")
+rmse = np.sqrt(mean_squared_error(y_test, regr.predict(X_test)))
+print("\n\n BoosterRegressor RMSE", rmse)
+
+
+datasets = [load_iris(return_X_y=True),
+            load_breast_cancer(return_X_y=True),
+            load_wine(return_X_y=True)]
+datasets_names = ['iris', 'breast_cancer', 'wine']
+
+# Booster
+for dataset, dataset_name in zip(datasets, datasets_names):
+    print("\n data set ", dataset_name)
+    X, y = dataset
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    regr = BoosterClassifier(base_estimator=ExtraTreeRegressor())
+    start = time()
+    regr.fit(X_train, y_train)
+    end = time()
+    print(f"Time taken: {end - start} seconds")
+    accuracy = regr.score(X_test, y_test)
+    print("BoosterClassifier Accuracy", accuracy)
