989b23fd9e4768e1437bc3fd2098eac2976be7f2
Thierry Moudiki <thierry.moudiki@gmail.com>
2025-02-09 18:13:39 +0100

    import one hot encoder Pt.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="genbooster",
-    version="0.6.6",
+    version="0.6.7",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
--- src/genbooster/adaboostclassifier.py
+++ src/genbooster/adaboostclassifier.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import ExtraTreeRegressor
-from pythonutils import one_hot_encode2
 from .adaboostregressor import AdaBoostRegressor
 
 
@@ -147,3 +146,20 @@ class AdaBoostClassifier(BaseEstimator, ClassifierMixin):
         shifted_preds = raw_preds - np.max(raw_preds, axis=0)
         exp_preds = np.exp(shifted_preds)
         return exp_preds / np.sum(exp_preds, axis=0)
+
+# one-hot encoding
+def one_hot_encode2(y, n_classes):
+    # Convert pandas Series or DataFrame to numpy array
+    if hasattr(y, 'values'):
+        y = np.asarray(y.values, dtype=np.int64)
+    else:
+        y = np.asarray(y, dtype=np.int64)
+    
+    # Initialize the one-hot encoded matrix
+    res = np.zeros((len(y), n_classes))
+    
+    # Fill in the 1s
+    for i in range(len(y)):
+        res[i, y[i]] = 1
+        
+    return res
