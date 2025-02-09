2a65d8b2eb4849d0b665348ebb8658edfd6a0f8b
Thierry Moudiki <thierry.moudiki@gmail.com>
2025-02-09 18:02:04 +0100

    import one hot encoder Pt.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="genbooster",
-    version="0.6.3",
+    version="0.6.4",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
--- src/pythonutils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import one_hot_encode2
-
-__all__ = ['one_hot_encode2']
